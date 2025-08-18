15e15a1ef00901b25fd94ee75dc5c832cc1dce74
Kao, Shih-Chi <blackdiamondkao@gmail.com>
2025-08-18 15:42:37 +0800

    Refactor laser profile analysis

--- source codes/Analyze_CMD.m
+++ source codes/Analyze_CMD.m
@@ -9,34 +9,32 @@ E1_mJ = 8.3;      E1_J = E1_mJ * 1e-3;       % for input.tiff
 E2_mJ = 7.636;    E2_J = E2_mJ * 1e-3;       % for output.tiff
 
 % -------- Case 1: input.tiff --------
-lp1 = LaserProfileProcessor('input.tiff', 'background.tiff');
-lp1 = lp1.loadImages();
-lp1 = lp1.subtractBackground();
-lp1 = lp1.computeIntensity(pixel_m, E1_J, tau_s);
+[in1, bg] = LoadLaserImages('input.tiff', 'background.tiff');
+corr1 = SubtractBackground(in1, bg);
+[~, I1_Wcm2] = ComputePeakIntensity(corr1, pixel_m, E1_J, tau_s);
 
 % -------- Case 2: output.tiff (same background) --------
-lp2 = LaserProfileProcessor('output.tiff', 'background.tiff');
-lp2 = lp2.loadImages();
-lp2 = lp2.subtractBackground();
-lp2 = lp2.computeIntensity(pixel_m, E2_J, tau_s);
+[in2, bg] = LoadLaserImages('output.tiff', 'background.tiff');
+corr2 = SubtractBackground(in2, bg);
+[~, I2_Wcm2] = ComputePeakIntensity(corr2, pixel_m, E2_J, tau_s);
 
 % ---- Show both intensity maps side-by-side (W/cm^2) ----
 figure; tiledlayout(1,2, 'Padding','compact', 'TileSpacing','compact');
 
 nexttile;
-imagesc(lp1.intensity_Wcm2); axis image; colormap hot; cb1=colorbar;
+imagesc(I1_Wcm2); axis image; colormap hot; cb1=colorbar;
 cb1.Label.String = 'Peak Intensity (W/cm^2)';
 title(sprintf('input.tiff  (%.3f mJ)', E1_mJ));
 xlabel('X pixel'); ylabel('Y pixel');
 
 nexttile;
-imagesc(lp2.intensity_Wcm2); axis image; colormap hot; cb2=colorbar;
+imagesc(I2_Wcm2); axis image; colormap hot; cb2=colorbar;
 cb2.Label.String = 'Peak Intensity (W/cm^2)';
 title(sprintf('output.tiff (%.3f mJ)', E2_mJ));
 xlabel('X pixel'); ylabel('Y pixel');
 
 % ---- Print peak intensities to Command Window ----
-Ipk1 = max(lp1.intensity_Wcm2(:));
-Ipk2 = max(lp2.intensity_Wcm2(:));
+Ipk1 = max(I1_Wcm2(:));
+Ipk2 = max(I2_Wcm2(:));
 fprintf('Peak intensity (input.tiff, %.3f mJ):  %.3g W/cm^2\n',  E1_mJ, Ipk1);
 fprintf('Peak intensity (output.tiff, %.3f mJ): %.3g W/cm^2\n',  E2_mJ, Ipk2);
