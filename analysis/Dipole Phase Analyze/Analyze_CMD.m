0037d477ba1da06b5464ece346df622a9e74c13c
Kao, Shih-Chi <blackdiamondkao@gmail.com>
2025-08-27 14:30:10 +0800

    Modify git ignore

--- analysis/Dipole Phase Analyze/Analyze_CMD.m
+++ analysis/Dipole Phase Analyze/Analyze_CMD.m
@@ -15,7 +15,7 @@ tau_fs   = 40;                         % fs
 tau_s    = tau_fs * 1e-15;
 
 E1_mJ = 8.3;      E1_J = E1_mJ * 1e-3; % input.tiff
-E2_mJ = 7.636;    E2_J = E2_mJ * 1e-3; % output.tiff
+E2_mJ = 7.6;    E2_J = E2_mJ * 1e-3; % output.tiff
 
 q         = 47;                        % harmonic order
 lambda_nm = 808;                       % nm
@@ -26,7 +26,7 @@ I_p       = I_p_eV * q_e;              % J
 %% -------- Load images once --------
 bg  = im2double(imread('background.tiff'));
 in1 = im2double(imread('input.tiff'));
-in2 = im2double(imread('output.tiff'));
+in2 = im2double(imread('output_0.3psi.tiff'));
 
 % Background subtraction (clip at 0)
 corr1 = max(in1 - bg, 0);
